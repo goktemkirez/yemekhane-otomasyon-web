cfd1f77e29997a3f90d5c0c8f406c0e6d7bf69ca
Orçun Göktem Kirez <ogkirez@gmail.com>
2021-11-23 22:21:19 +0300

    recors page edited

--- src/pages/Records/Records.js
+++ src/pages/Records/Records.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import authAxios from "../../components/axios";
 import {
   Box,
   Button,
@@ -16,81 +15,34 @@ import {
 import { Skeleton } from "@material-ui/lab";
 import { AddOutlined } from "@material-ui/icons";
 
+import authAxios from "../../components/axios";
 import { useStyles } from "./Records.style";
 
 function Records() {
   const classes = useStyles();
   const [loading, setLoading] = useState(false);
-  const [userListData, setUserListData] = useState([]);
+  const [recordList, setRecordList] = useState([]);
 
   useEffect(() => {
-    getUserListData();
+    getRecordList();
   }, []);
 
-  const getUserListData = async () => {
+  const getRecordList = async () => {
     try {
       setLoading(true);
 
-      const result = await authAxios.get(`/users`);
-      console.log(result.data.items);
-      setUserListData(result.data.items);
+      const result = await authAxios.get(`/records`);
+      console.log(result.data);
+      setRecordList(result.data);
     } catch (error) {
       console.log("error", error);
     } finally {
-      setTimeout(() => {
-        setLoading(false);
-      }, 1000);
-    }
-  };
-
-  const createPlaylist = async () => {
-    try {
-      const result = await authAxios.get(`/me`);
-      const response = await authAxios.post(
-        `/users/${result.data.id}/playlists`,
-        {
-          name: "New Playlist",
-          description: "New playlist description",
-          public: false,
-        }
-      );
-      console.log(response.data);
-    } catch (error) {
-      console.log("error", error);
-    } finally {
-    }
-  };
-
-  const removeTracks = async (playlistId) => {
-    // https://developer.spotify.com/documentation/general/guides/working-with-playlists/#following-and-unfollowing-a-playlist
-    // they say "We have no endpoint for deleting a playlist in the Web API"
-    // Burası biraz karışık şimdilik dokunma başka bir şeye bakalım
-    // ama eğer ki karta bi fonksiyon koycaksam ve bu fonksiyon da o playlistin id'si ile request atıcaksa bu şekilde implement etmek gerekiyor
-    try {
-      const response = await authAxios.delete(
-        `/playlists/${playlistId}/tracks`
-      );
-      console.log(response.data);
-    } catch (error) {
-      console.log("error", error);
-    } finally {
+      setLoading(false);
     }
   };
 
   return (
     <Box>
-      <Box className={classes.topBox}>
-        <Typography variant="h5">Playlists</Typography>
-        <Button
-          variant="contained"
-          color="secondary"
-          className={classes.button}
-          startIcon={<AddOutlined />}
-          onClick={createPlaylist}
-        >
-          Create Playlist
-        </Button>
-      </Box>
       <Container maxWidth="xl" className={classes.containerStyle}>
         {loading ? (
           <Box>
@@ -104,40 +56,24 @@ function Records() {
               <Table className={classes.table} aria-label="simple table">
                 <TableHead>
                   <TableRow>
-                    <TableCell>Dessert (100g serving)</TableCell>
-                    <TableCell align="right">Calories</TableCell>
-                    <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                    <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                    <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                    <TableCell>ID</TableCell>
+                    <TableCell align="right">User Id</TableCell>
+                    <TableCell align="right">Created Date - Time</TableCell>
                   </TableRow>
                 </TableHead>
                 <TableBody>
-                  {userListData.map((row) => (
+                  {recordList.map((row) => (
                     <TableRow key={row.id}>
                       <TableCell component="th" scope="row">
-                        {row.external_id}
+                        {row.id}
                       </TableCell>
-                      <TableCell align="right">{row.firstname}</TableCell>
-                      <TableCell align="right">{row.lastname}</TableCell>
+                      <TableCell align="right">{row.user_id}</TableCell>
+                      <TableCell align="right">{row.created_at}</TableCell>
                     </TableRow>
                   ))}
                 </TableBody>
               </Table>
             </TableContainer>
-
-            {/* {userListData.map((userData) => (
-              <PlaylistCard
-                key={userData.id}
-                owner={userData?.owner?.display_name} // Burda neden template literal yaptın direk bu şekilde de geçebilrisin propları
-                img={userData?.images[0]?.url} // Olaylara hep ben bununla bişey yaparım ilerde diye bakıyom :D
-                name={userData?.name}
-                urlLink={userData?.external_urls?.spotify}
-                tracksUrl={userData.id}
-                onDeleteClick={() => {
-                  removeTracks(userData.id);
-                }}
-              />
-            ))} */}
           </>
         )}
       </Container>
