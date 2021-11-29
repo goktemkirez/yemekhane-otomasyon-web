8e5a12dba4e21ce72f4eb966acbaff4ddd039dc6
goktemkirez <ogkirez@gmail.com>
2021-11-29 16:08:31 +0300

    baseURL updated

--- src/components/axios.js
+++ src/components/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const authAxios = axios.create({
   // baseURL: "http://192.168.1.37:5499/",
-  baseURL: "http://192.168.0.14:5499/",
+  baseURL: "https://yemekhane-be.herokuapp.com/",
   // headers: {
   //   Authorization: `Bearer ${API_KEY}`,
   // },
